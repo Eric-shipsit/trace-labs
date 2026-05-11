7a8e765105bfbbdc7bbb91c045dc6dc10f1c3a34
Eric-shipsit <EricL.swe@gmail.com>
2026-05-11 00:58:40 -0700

    fixing auth

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "./lib/prisma";
 import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
 
   session: {
